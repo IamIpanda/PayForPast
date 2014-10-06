e713b427823bf03fe60dacdd9cf77043289eca53
IamI <xinguangyao@gmail.com>
2014-10-06 09:59:41 +0800

    Initial Commit

---
+++ 为过去赎罪/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace 为过去赎罪
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(358, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 379);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(90, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 326);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "锁定底色";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(9, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(343, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(116, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 18);
+            this.label1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 379);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "所以，来为过去赎罪吧。";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 为过去赎罪/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 为过去赎罪
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo dir;
+        List<FileInfo> files = new List<FileInfo>();
+        List<String> patterns = new List<String> { ".jpg", ".jpeg", ".bmp", ".png" };
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") return;
+            dir = new DirectoryInfo(textBox1.Text);
+            button1.Enabled = dir.Exists;
+            listBox1.Items.Clear();
+            if (dir.Exists)
+            {
+                files.Clear();
+                foreach (var file in dir.GetFiles())
+                    if (patterns.Contains(file.Extension))
+                    {
+                        listBox1.Items.Add(file.Name);
+                        files.Add(file);
+                    }
+            }
+            else listBox1.Items.Add("路径不合法。");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled = 
+            label2.Enabled = checkBox1.Checked;
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < 0) return;
+            if (button1.Enabled == false) return;
+            FileInfo file = files[listBox1.SelectedIndex];
+            try
+            {
+                Bitmap bit = new Bitmap(file.FullName);
+                pictureBox1.Image = bit;
+            }
+            catch (Exception ex) { }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            Image bit = pictureBox1.Image;
+            double zoom, zoom_x, zoom_y, location_x, location_y;
+            zoom_x = bit.Width / (pictureBox1.Width + 0.0);
+            zoom_y = bit.Height / (pictureBox1.Height + 0.0);
+            if (zoom_x > zoom_y)
+            {
+                zoom = zoom_x;
+                location_x = 0;
+                location_y = (pictureBox1.Height - bit.Height / zoom_x) / 2;
+            }
+            else
+            {
+                zoom = zoom_y;
+                location_y = 0;
+                location_x = (pictureBox1.Width - bit.Width / zoom_y) / 2;
+            }
+            location_x = (e.X - location_x) * zoom;
+            location_y = (e.Y - location_y) * zoom;
+            Color color = ((Bitmap)bit).GetPixel((int)location_x, (int)location_y);
+            label2.BackColor = color;
+            label1.Text = color.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            new System.Threading.Thread(this.Work).Start();
+        }
+        public void Work()
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+            Bitmap bit, clone;
+            for (int i = 0; i < files.Count; i++)
+            {
+                FileInfo file = files[i];
+                this.listBox1.SelectedIndex = i;
+                bit = null;
+                try
+                {
+                    bit = new Bitmap(file.FullName);
+                }
+                catch (Exception ex) { }
+                if (bit == null) continue;
+                Color back;
+                if (checkBox1.Checked) back = label2.BackColor;
+                else
+                {
+                    back = bit.GetPixel(0, 0);
+                    label2.BackColor = back;
+                    label1.Text = back.ToString();
+                }
+                clone = bit.Clone() as Bitmap;
+                bit.Dispose();
+                clone.MakeTransparent(back);
+                Bitmap show = new Bitmap(clone.Width, clone.Height);
+                Graphics gra = Graphics.FromImage(show);
+                gra.DrawImage(clone, 0, 0);
+                gra.Save();
+                pictureBox1.Image = show;
+                try
+                {
+                    clone.Save(file.FullName);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            MessageBox.Show("完成了您的操作");
+            Application.Exit();
+        }
+
+        private void Invoke(Action<int> action, int i)
+        {
+            action.Invoke(i);
+        }
+        public void setIndex(int i)
+        {
+            this.listBox1.SelectedIndex = i;
+        }
+
+    }
+}
